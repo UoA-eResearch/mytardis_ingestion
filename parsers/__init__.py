1183c0d2d7a1191f4bf07196cf56e98c8ba9ce1e
Chris Seal <c.seal@auckland.ac.nz>
2020-03-04 09:16:17 +1300

    Debugging

--- parsers/__init__.py
+++ parsers/__init__.py
@@ -1,8 +1,8 @@
 __version__ = '0.10'
 __author__ = 'Chris Seal <c.seal@auckland.ac.nz>'
 # helper to minimise confusion with loading parsers
-from .parser import Parser
-from .csv_parser import *
-from .genomics_print import *
+from .parser import *
+#from csv_parser import *
+#from genomics_print import *
 #from .dir_parser import *
 from .solarix_parser import *
