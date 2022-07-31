3facaf7f3adc9bb72f3830d4a5963e39b0013bfa
Chris Seal <c.seal@auckland.ac.nz>
2022-08-01 10:20:08 +1200

    latest tests

--- tests/test_overseers.py
+++ tests/test_overseers.py
@@ -11,53 +11,53 @@ from pytest import fixture
 from responses import matchers
 
 from src.blueprints import StorageBox
+from src.blueprints.custom_data_types import URI
 from src.helpers.config import AuthConfig, ConnectionConfig, IntrospectionConfig
+from src.helpers.enumerators import ObjectSearchEnum
 from src.overseers import Overseer
+
+from .conftest import connection, overseer, project_response_dict
 
 logger = logging.getLogger(__name__)
 logger.propagate = True
 
 
-@fixture
-@responses.activate
-def overseer(
-    auth: AuthConfig,
-    connection: ConnectionConfig,
-    mytardis_setup: IntrospectionConfig,
-) -> Overseer:
-    return Overseer(auth, connection, mytardis_setup)
-
-
 def test_staticmethod_resource_uri_to_id():
-    test_uri = "/api/v1/user/10/"
+    test_uri = URI("/v1/user/10/")
     assert Overseer.resource_uri_to_id(test_uri) == 10
 
 
 @responses.activate
 def test_get_objects(
-    connection: ConnectionConfig,
-    overseer: Overseer,
-    project_response_dict,
+    overseer: Overseer, connection: ConnectionConfig, project_response_dict
 ):
-    object_type = "project"
-    search_target = "pids"
+    object_type = ObjectSearchEnum.PROJECT.value
     search_string = "Project_1"
     responses.add(
         responses.GET,
-        urljoin(connection.api_template, object_type),
+        urljoin(connection.api_template, object_type["type"]),
         json=(project_response_dict),
         match=[
             matchers.query_param_matcher(
-                {search_target: search_string},
+                {object_type["target"]: search_string},
             ),
         ],
         status=200,
     )
-
+    responses.add(
+        responses.GET,
+        urljoin(connection.api_template, object_type["type"]),
+        json=(project_response_dict),
+        match=[
+            matchers.query_param_matcher(
+                {"pids": search_string},
+            ),
+        ],
+        status=200,
+    )
     assert (
         overseer.get_objects(
             object_type,
-            search_target,
             search_string,
         )
         == project_response_dict["objects"]
@@ -70,15 +70,24 @@ def test_get_objects_http_error(
     connection: ConnectionConfig,
     overseer: Overseer,
 ):
-    object_type = "project"
-    search_target = "pids"
+    object_type = ObjectSearchEnum.PROJECT.value
     search_string = "Project_1"
     responses.add(
         responses.GET,
-        urljoin(connection.api_template, object_type),
+        urljoin(connection.api_template, object_type["type"]),
         match=[
             matchers.query_param_matcher(
-                {search_target: search_string},
+                {object_type["target"]: search_string},
+            ),
+        ],
+        status=504,
+    )
+    responses.add(
+        responses.GET,
+        urljoin(connection.api_template, object_type["type"]),
+        match=[
+            matchers.query_param_matcher(
+                {"pids": search_string},
             ),
         ],
         status=504,
@@ -87,13 +96,11 @@ def test_get_objects_http_error(
     warning_str = (
         "Failed HTTP request from Overseer.get_objects call\n"
         f"object_type = {object_type}\n"
-        f"search_target = {search_target}\n"
-        f"search_string = {search_string}"
+        "query_params"
     )
     assert (
         overseer.get_objects(
             object_type,
-            search_target,
             search_string,
         )
         is None
@@ -108,19 +115,16 @@ def test_get_objects_general_error(
     overseer: Overseer,
 ):
     mock_mytardis_api_request.side_effect = IOError()
-    object_type = "project"
-    search_target = "pids"
+    object_type = ObjectSearchEnum.PROJECT.value
     search_string = "Project_1"
     error_str = (
         "Non-HTTP exception in Overseer.get_objects call\n"
         f"object_type = {object_type}\n"
-        f"search_target = {search_target}\n"
-        f"search_string = {search_string}"
+        "query_params"
     )
     with pytest.raises(IOError):
         _ = overseer.get_objects(
             object_type,
-            search_target,
             search_string,
         )
         assert error_str in caplog.text
@@ -128,20 +132,30 @@ def test_get_objects_general_error(
 
 @responses.activate
 def test_get_objects_no_objects(
-    connection: ConnectionConfig,
     overseer: Overseer,
+    connection: ConnectionConfig,
     response_dict_not_found,
 ):
-    object_type = "project"
-    search_target = "pids"
+    object_type = ObjectSearchEnum.PROJECT.value
     search_string = "Project_1"
     responses.add(
         responses.GET,
-        urljoin(connection.api_template, object_type),
+        urljoin(connection.api_template, object_type["type"]),
         json=(response_dict_not_found),
         match=[
             matchers.query_param_matcher(
-                {search_target: search_string},
+                {object_type["target"]: search_string},
+            ),
+        ],
+        status=200,
+    )
+    responses.add(
+        responses.GET,
+        urljoin(connection.api_template, object_type["type"]),
+        json=(response_dict_not_found),
+        match=[
+            matchers.query_param_matcher(
+                {"pids": search_string},
             ),
         ],
         status=200,
@@ -150,10 +164,9 @@ def test_get_objects_no_objects(
     assert (
         overseer.get_objects(
             object_type,
-            search_target,
             search_string,
         )
-        == []
+        is None
     )
 
 
@@ -163,25 +176,34 @@ def test_get_uris(
     overseer: Overseer,
     project_response_dict,
 ):
-    object_type = "project"
-    search_target = "pids"
+    object_type = ObjectSearchEnum.PROJECT.value
     search_string = "Project_1"
     responses.add(
         responses.GET,
-        urljoin(connection.api_template, object_type),
+        urljoin(connection.api_template, object_type["type"]),
         json=(project_response_dict),
         match=[
             matchers.query_param_matcher(
-                {search_target: search_string},
+                {object_type["target"]: search_string},
+            ),
+        ],
+        status=200,
+    )
+    responses.add(
+        responses.GET,
+        urljoin(connection.api_template, object_type["type"]),
+        json=(project_response_dict),
+        match=[
+            matchers.query_param_matcher(
+                {"pids": search_string},
             ),
         ],
         status=200,
     )
     assert overseer.get_uris(
         object_type,
-        search_target,
         search_string,
-    ) == ["/api/v1/project/1/"]
+    ) == [URI("/api/v1/project/1/")]
 
 
 @responses.activate
@@ -190,16 +212,26 @@ def test_get_uris_no_objects(
     overseer: Overseer,
     response_dict_not_found,
 ):
-    object_type = "project"
-    search_target = "pids"
+    object_type = ObjectSearchEnum.PROJECT.value
     search_string = "Project_1"
     responses.add(
         responses.GET,
-        urljoin(connection.api_template, object_type),
+        urljoin(connection.api_template, object_type["type"]),
         json=(response_dict_not_found),
         match=[
             matchers.query_param_matcher(
-                {search_target: search_string},
+                {object_type["target"]: search_string},
+            ),
+        ],
+        status=200,
+    )
+    responses.add(
+        responses.GET,
+        urljoin(connection.api_template, object_type["type"]),
+        json=(response_dict_not_found),
+        match=[
+            matchers.query_param_matcher(
+                {"pids": search_string},
             ),
         ],
         status=200,
@@ -207,10 +239,9 @@ def test_get_uris_no_objects(
     assert (
         overseer.get_uris(
             object_type,
-            search_target,
             search_string,
         )
-        == []
+        is None
     )
 
 
@@ -224,28 +255,37 @@ def test_get_uris_malformed_return_dict(
     caplog.set_level(logging.ERROR)
     test_dict = project_response_dict
     test_dict["objects"][0].pop("resource_uri")
-    object_type = "project"
-    search_target = "pids"
+    object_type = ObjectSearchEnum.PROJECT.value
     search_string = "Project_1"
     responses.add(
         responses.GET,
-        urljoin(connection.api_template, object_type),
-        json=(project_response_dict),
+        urljoin(connection.api_template, object_type["type"]),
+        json=(test_dict),
         match=[
             matchers.query_param_matcher(
-                {search_target: search_string},
+                {object_type["target"]: search_string},
+            ),
+        ],
+        status=200,
+    )
+    responses.add(
+        responses.GET,
+        urljoin(connection.api_template, object_type["type"]),
+        json=(test_dict),
+        match=[
+            matchers.query_param_matcher(
+                {"pids": search_string},
             ),
         ],
         status=200,
     )
     error_str = (
         "Malformed return from MyTardis. No resource_uri found for "
-        f"{object_type} searching on {search_target} with {search_string}"
+        f"{object_type} searching with {search_string}"
     )
     with pytest.raises(KeyError):
         _ = overseer.get_uris(
             object_type,
-            search_target,
             search_string,
         )
         assert error_str in caplog.text
@@ -258,15 +298,24 @@ def test_get_uris_ensure_http_errors_caught_by_get_objects(
     overseer: Overseer,
 ):
     caplog.set_level(logging.WARNING)
-    object_type = "project"
-    search_target = "pids"
+    object_type = ObjectSearchEnum.PROJECT.value
     search_string = "Project_1"
     responses.add(
         responses.GET,
-        urljoin(connection.api_template, object_type),
+        urljoin(connection.api_template, object_type["type"]),
         match=[
             matchers.query_param_matcher(
-                {search_target: search_string},
+                {object_type["target"]: search_string},
+            ),
+        ],
+        status=504,
+    )
+    responses.add(
+        responses.GET,
+        urljoin(connection.api_template, object_type["type"]),
+        match=[
+            matchers.query_param_matcher(
+                {"pids": search_string},
             ),
         ],
         status=504,
@@ -274,13 +323,11 @@ def test_get_uris_ensure_http_errors_caught_by_get_objects(
     warning_str = (
         "Failed HTTP request from Overseer.get_objects call\n"
         f"object_type = {object_type}\n"
-        f"search_target = {search_target}\n"
-        f"search_string = {search_string}"
+        "query_params"
     )
     assert (
         overseer.get_uris(
             object_type,
-            search_target,
             search_string,
         )
         is None
@@ -303,92 +350,3 @@ def test_get_uris_general_error(
             search_target,
             search_string,
         )
-
-
-def test_is_uri_true():
-    object_type = "project"
-    uri_string = f"/api/v1/{object_type}/1/"
-    assert Overseer.is_uri(uri_string, object_type)
-
-
-def test_is_uri_false_text_before_uri():
-    object_type = "project"
-    uri_string = f"http://test.com/api/v1/{object_type}/1/"
-    assert Overseer.is_uri(uri_string, object_type) is False
-
-
-def test_is_uri_false_text_after_uri():
-    object_type = "project"
-    uri_string = f"/api/v1/{object_type}/1/edit/"
-    assert Overseer.is_uri(uri_string, object_type) is False
-
-
-def test_is_uri_false():
-    object_type = "project"
-    uri_string = "Some other text"
-    assert Overseer.is_uri(uri_string, object_type) is False
-
-
-def test_is_uri_int_not_string():
-    object_type = "project"
-    uri_string = 1
-    assert Overseer.is_uri(uri_string, object_type) is False
-
-
-@responses.activate
-def test_get_uris_by_identifier(
-    connection: ConnectionConfig,
-    overseer: Overseer,
-    project_response_dict,
-):
-    object_type = "project"
-    search_target = "pids"
-    search_string = "Project_1"
-    responses.add(
-        responses.GET,
-        urljoin(connection.api_template, object_type),
-        json=(project_response_dict),
-        match=[
-            matchers.query_param_matcher(
-                {search_target: search_string},
-            ),
-        ],
-        status=200,
-    )
-    assert overseer.get_uris_by_identifier(
-        object_type,
-        search_string,
-    ) == ["/api/v1/project/1/"]
-
-
-def test_get_uris_by_identifier_app_not_used(
-    caplog,
-    overseer: Overseer,
-):
-    caplog.set_level(logging.WARNING)
-    object_type = "project"
-    search_string = "Project_1"
-    overseer.mytardis_setup.objects_with_ids = []
-    warning_str = (
-        "The identifiers app is not installed in the instance of MyTardis, "
-        "or there are no objects defined in OBJECTS_WITH_IDENTIFIERS in "
-        "settings.py"
-    )
-    assert overseer.get_uris_by_identifier(object_type, search_string) is None
-    assert warning_str in caplog.text
-
-
-def test_get_uris_by_identifier_object_not_set_up_for_ids(
-    caplog,
-    overseer: Overseer,
-):
-    caplog.set_level(logging.WARNING)
-    object_type = "project"
-    search_string = "Project_1"
-    overseer.mytardis_setup.objects_with_ids = ["experiment"]
-    warning_str = (
-        f"The object type, {object_type}, is not present in "
-        "OBJECTS_WITH_IDENTIFIERS defined in settings.py"
-    )
-    assert overseer.get_uris_by_identifier(object_type, search_string) is None
-    assert warning_str in caplog.text
