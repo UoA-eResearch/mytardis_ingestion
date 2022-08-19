f6214a7c95b4e03b6b269b860acbf7c70a8db9cb
Lukas Trombach <lukas.trombach@auckland.ac.nz>
2022-08-19 14:45:47 +1200

    separate config and responses fixtures

---
+++ tests/fixtures/fixtures_config_from_env.py
@@ -0,0 +1,87 @@
+from pytest import fixture
+
+from src.helpers.config import (
+    GeneralConfig,
+    AuthConfig,
+    ConnectionConfig,
+    StorageConfig,
+    SchemaConfig,
+    ProxyConfig,
+    IntrospectionConfig,
+    ConfigFromEnv,
+)
+
+
+@fixture
+def general(default_institution) -> GeneralConfig:
+    return GeneralConfig(default_institution=default_institution)
+
+
+@fixture
+def auth(username, api_key) -> AuthConfig:
+    return AuthConfig(username=username, api_key=api_key)
+
+
+@fixture
+def connection(hostname, proxies, verify_certificate) -> ConnectionConfig:
+    return ConnectionConfig(
+        hostname=hostname,
+        proxy=ProxyConfig(http=proxies["http"], https=proxies["https"]),
+        verify_certificate=verify_certificate,
+    )
+
+
+@fixture
+def storage(storage_box, source_dir, target_dir) -> StorageConfig:
+    return StorageConfig(
+        box=storage_box.name,
+        source_directory=source_dir,
+        target_directory=target_dir,
+    )
+
+
+@fixture
+def default_schema(
+    project_schema, experiment_schema, dataset_schema, datafile_schema
+) -> SchemaConfig:
+    return SchemaConfig(
+        project=project_schema,
+        experiment=experiment_schema,
+        dataset=dataset_schema,
+        datafile=datafile_schema,
+    )
+
+
+@fixture
+def mytardis_setup(processed_introspection_response) -> IntrospectionConfig:
+    return IntrospectionConfig(
+        old_acls=processed_introspection_response["old_acls"],
+        projects_enabled=processed_introspection_response["projects_enabled"],
+        objects_with_ids=processed_introspection_response["objects_with_ids"],
+    )
+
+
+@fixture
+def mytardis_settings_no_introspection(
+    general: GeneralConfig,
+    auth: AuthConfig,
+    connection: ConnectionConfig,
+    storage: StorageConfig,
+    default_schema: SchemaConfig,
+) -> ConfigFromEnv:
+    return ConfigFromEnv(
+        general=general,
+        auth=auth,
+        connection=connection,
+        storage=storage,
+        default_schema=default_schema,
+    )
+
+
+@fixture
+def mytardis_settings(
+    mytardis_settings_no_introspection: ConfigFromEnv,
+    mytardis_setup: IntrospectionConfig,
+) -> ConfigFromEnv:
+    mytardis_settings_no_introspection._mytardis_setup = mytardis_setup
+    return mytardis_settings_no_introspection
