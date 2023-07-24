29ae02ab7a35b2df58553bb90bd83dc28e06fc4c
Chris Seal <c.seal@auckland.ac.nz>
2023-07-25 11:41:04 +1200

    Add Baseclasses back for enumerations

--- src/blueprints/__init__.py
+++ src/blueprints/__init__.py
@@ -3,24 +3,28 @@
 from src.blueprints.common_models import GroupACL, Parameter, ParameterSet, UserACL
 from src.blueprints.custom_data_types import URI, ISODateTime, MTUrl, Username
 from src.blueprints.datafile import (
+    BaseDatafile,
     Datafile,
     DatafileReplica,
     RawDatafile,
     RefinedDatafile,
 )
 from src.blueprints.dataset import (
+    BaseDataset,
     Dataset,
     DatasetParameterSet,
     RawDataset,
     RefinedDataset,
 )
 from src.blueprints.experiment import (
+    BaseExperiment,
     Experiment,
     ExperimentParameterSet,
     RawExperiment,
     RefinedExperiment,
 )
 from src.blueprints.project import (
+    BaseProject,
     Project,
     ProjectParameterSet,
     RawProject,
