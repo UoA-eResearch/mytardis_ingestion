1bd41c3eb2b56fd9f26dec5251a3210ddeb609a8
Chris Seal <c.seal@auckland.ac.nz>
2023-09-14 16:54:43 +1200

    style(Append tests to prevent bandit from rejecting asserts): Add exclusions to Bandit for tests

--- tests/test_mytardis_ingestion.py
+++ tests/test_mytardis_ingestion.py
@@ -1,6 +1,7 @@
 # pylint: disable=missing-module-docstring,missing-function-docstring
+
 from src import __version__
 
 
-def test_version():
+def test_version() -> None:
     assert __version__ == "0.8.0"
