7f883da36281d779d334bcfecbce0f4d68a718fa
Chris Seal <c.seal@auckland.ac.nz>
2023-10-04 14:42:36 +1300

    Fix Prospector

--- src/prospectors/prospector.py
+++ src/prospectors/prospector.py
@@ -1,18 +1,18 @@
 """Performs the prospecting process
 
 Prospecting checks files and metadata files for any potential issues.
-
-Specific prospector classes need to be subclassed from the abstract Prospector class
 """
 
-import logging
-
 # ---Imports
-from abc import ABC, abstractmethod
+import copy
+import logging
 from pathlib import Path
-from typing import Optional
+from typing import Optional, Union
 
-from src.extraction_output_manager.output_manager import OutputManager
+from src.config.singleton import Singleton
+from src.extraction_output_manager import output_manager as om
+from src.prospectors.abstract_custom_prospector import AbstractCustomProspector
+from src.prospectors.common_file_checks import CommonDirectoryTreeChecks
 
 # ---Constants
 logger = logging.getLogger(__name__)
@@ -20,21 +20,18 @@ logger.setLevel(logging.DEBUG)
 
 
 # ---Code
-class AbstractProspector(ABC):
-    """Abstract base class for the Prospector is used to prospect a given directory for files.
+class Prospector(metaclass=Singleton):
+    """This class Prospector is used to prospect a given directory for files.
 
     Attributes:
         profile_sel (profile_selector.ProfileSelector): selected profile module
     """
 
-    @abstractmethod
-    def prospect(
+    def __init__(
         self,
-        path: Path,
-        recursive: bool,
-        out_man: OutputManager,
+        custom_prospector: Union[AbstractCustomProspector, None],
     ) -> None:
-        """Prospects metadata in a path"""
+        self.custom_prospector = custom_prospector
 
     def prospect_directory(
         self,
@@ -51,6 +48,29 @@ class AbstractProspector(ABC):
             out_man (om.OutputManager): Output manager instance.
 
         Returns:
-            OutputManager: output manager instance containing the outputs of the process
+            om.OutputManager: Output manager instance.
         """
-        return None
+
+        if not out_man:
+            out_man = om.OutputManager()
+
+        cmn_dir_tree_c = CommonDirectoryTreeChecks()
+
+        out = cmn_dir_tree_c.perform_common_file_checks(path, recursive)
+        rej_list = out[0]
+        out_man.add_files_to_ignore(rej_list)
+
+        if self.custom_prospector:
+            out_man_fnl = self.custom_prospector.prospect(path, recursive, out_man)
+        else:
+            out_man_fnl = out_man
+            logger.info("No custom prospector set, thus will not be used")
+
+        logger.info("prospecting complete")
+        logger.info(f"ignored dirs = {out_man_fnl.dirs_to_ignore}")
+        logger.info(f"ignored files = {out_man_fnl.files_to_ignore}")
+        logger.info(f"files to ingest = {out_man_fnl.metadata_files_to_ingest_dict}")
+        logger.info(f"output dict = {out_man_fnl.output_dict}")
+
+        return out_man_fnl
+"
