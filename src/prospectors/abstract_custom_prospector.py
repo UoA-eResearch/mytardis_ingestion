56d80bc84cde40de81c4600768862c082f45e30a
Chris Seal <c.seal@auckland.ac.nz>
2023-10-04 15:01:29 +1300

    Pylinting abstract ustom prospector

--- src/prospectors/abstract_custom_prospector.py
+++ src/prospectors/abstract_custom_prospector.py
@@ -3,15 +3,12 @@
 
 
 # ---Imports
-import copy
 import logging
-import os
 from abc import ABC, abstractmethod
 from pathlib import Path
-from typing import Any, Dict, Optional
+from typing import Optional
 
 from src.extraction_output_manager import output_manager as om
-from src.profiles import profile_consts as pc
 
 # ---Constants
 logger = logging.getLogger(__name__)
@@ -19,7 +16,7 @@ logger.setLevel(logging.DEBUG)  # set the level for which this logger will be pr
 
 
 # ---Code
-class AbstractCustomProspector:
+class AbstractCustomProspector(ABC):
     """Profile-specific prospector class
 
     Each profile has a custom prospector whose behaviour is based on the
@@ -44,9 +41,10 @@ class AbstractCustomProspector:
         Args:
             path (Path): the path to inspect for metadata
             recursive (bool): True to inspect all subdirectories in path, False to inspect path only
-            out_man (om.OutputManager): class which stores info of outputs of the pre-ingestion processes
+            out_man (om.OutputManager): class which stores info of outputs of the pre-ingestion
+                processes
 
         Returns:
             om.OutputManager: output manager instance containing the outputs of the process
         """
-        pass
+        return om.OutputManager()
