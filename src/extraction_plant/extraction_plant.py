0503746868b4fe9a2f1fd263cb3d2ab9522bc9fa
LibbyLi667 <libbyjuan69@gmail.com>
2023-10-26 14:55:31 +1300

    fix mypy and pylint

--- src/extraction_plant/extraction_plant.py
+++ src/extraction_plant/extraction_plant.py
@@ -9,7 +9,7 @@ import logging
 from pathlib import Path
 from typing import Any, Dict, Optional, Union
 
-from src.beneficiations.beneficiation import Beneficiation # type: src.beneficiations.beneficiation.Beneficiation
+from src.beneficiations.beneficiation import Beneficiation  # type: src.beneficiations.beneficiation.Beneficiation
 from src.config.singleton import Singleton
 from src.extraction_output_manager.ingestibles import IngestibleDataclasses
 from src.extraction_output_manager.output_manager import OutputManager
