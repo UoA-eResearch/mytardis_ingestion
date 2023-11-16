fa8464b28f0e822167ec6a17c8f93c2b540f8a04
andrew-uoa <142769327+andrew-uoa@users.noreply.github.com>
2023-11-17 12:27:47 +1300

    IDS-281: Generate ABI dataclasses from JSON files (#337)
    * Delete outdated config file template
    
    * Initial implementation of parsing project info for ABI Music
    
    * Preliminary implementation of experiment and dataset json parsing
    
    * Populate some more fields of the raw dataset (with placeholders for now)
    
    * Extract the timestamp-parsing to a function
    
    * Tidy up comments, add instrument ID, and extract minimal dataset metadata from JSON
    
    * Update some comments
    
    * Sort imports correctly
    
    * Fix pylint issues (mainly line length)
    
    * Declare the default institution explicitly as a constant
    
    * Remove directory from dataset declaration. It's not clear we need it at this stage

---
+++ src/profiles/abi_music/parsing.py
@@ -0,0 +1,193 @@
+"""
+Parsing logic for generating PEDD dataclasses from ABI Music files
+"""
+
+import json
+import re
+from datetime import datetime
+from pathlib import Path
+from typing import Any
+
+from src.blueprints.common_models import GroupACL
+from src.blueprints.custom_data_types import MTUrl
+from src.blueprints.dataset import RawDataset
+from src.blueprints.experiment import RawExperiment
+from src.blueprints.project import RawProject
+from src.helpers.enumerators import DataClassification
+
+# Expected datetime format is "yymmdd-DDMMSS"
+datetime_pattern = re.compile("^[0-9]{6}-[0-9]{6}$")
+
+DEFAULT_INSTITUTION = "University of Auckland"
+
+
+def parse_timestamp(timestamp: str) -> datetime:
+    """
+    Parse a timestamp string in the ABI Music format: yymmdd-DDMMSS
+
+    Returns a datetime object or raises a ValueError if the string is ill-formed.
+    """
+    # strptime is a bit too lenient with its input format, so pre-validate with a regex
+    if _ := datetime_pattern.match(timestamp):
+        return datetime.strptime(timestamp, r"%y%m%d-%H%M%S")
+
+    raise ValueError("Ill-formed timestamp; expected format 'yymmdd-DDMMSS'")
+
+
+def parse_project_info(json_data: dict[str, Any]) -> RawProject:
+    """
+    Extract project metadata from JSON content
+    """
+    groups: list[GroupACL] = []
+
+    for group in json_data["groups"]:
+        name = group["name"]
+        is_admin = group.get("admin") is True
+
+        if is_admin:
+            is_owner, can_download, see_sensitive = True, True, True
+        else:
+            is_owner = group.get("is_owner") is True
+            can_download = group.get("can_download") is True
+            see_sensitive = group.get("see_sensitive") is True
+
+        group_info = GroupACL(
+            group=name,
+            is_owner=is_owner,
+            can_download=can_download,
+            see_sensitive=see_sensitive,
+        )
+        groups.append(group_info)
+
+    raw_project = RawProject(
+        name=json_data["project_name"],
+        description=json_data["project_description"],
+        principal_investigator=json_data["principal_investigator"],
+        data_classification=DataClassification.SENSITIVE,
+        created_by=json_data["principal_investigator"],
+        users=None,
+        groups=groups,
+        identifiers=json_data["project_ids"],
+        institution=[DEFAULT_INSTITUTION],
+        metadata=None,
+        schema=None,
+        start_time=None,
+        end_time=None,
+        embargo_until=None,
+        active_stores=None,
+        archives=None,
+        delete_in_days=-1,
+        archive_in_days=365,
+    )
+
+    return raw_project
+
+
+def parse_experiment_info(json_data: dict[str, Any]) -> RawExperiment:
+    """
+    Extract experiment metadata from JSON content
+    """
+    raw_experiment = RawExperiment(
+        title=json_data["experiment_name"],
+        description=json_data["experiment_description"],
+        data_classification=None,
+        created_by=None,
+        url=None,
+        locked=False,
+        users=None,
+        groups=None,
+        identifiers=json_data["experiment_ids"],
+        projects=[json_data["project"]],
+        institution_name=None,
+        metadata=None,
+        schema=None,
+        start_time=None,
+        end_time=None,
+        created_time=None,
+        update_time=None,
+        embargo_until=None,
+    )
+
+    return raw_experiment
+
+
+def parse_dataset_info(json_data: dict[str, Any]) -> RawDataset:
+    """
+    Extract dataset metadata from JSON content
+    """
+
+    # These will be used separately when we implement the directory-parsing logic
+    abi_music_microscope = "abi-music-microscope-v1"
+    # abi_music_post_processing = "abi-music-post-processing-v1"
+
+    # These will be used separately when we implement the directory-parsing logic
+    dataset_schema_raw = MTUrl("http://abi-music.com/dataset-raw/1")
+    #  dataset_schema_zarr = MTUrl("http://abi-music.com/dataset-zarr/1")
+
+    # Currently taking the same fields for Raw and Zarr datasets, but that may change
+    metadata: dict[str, Any] = {
+        "description": json_data["Description"],
+        "sequence-id": json_data["SequenceID"],
+        "sqrt-offset": json_data["Offsets"]["SQRT Offset"],
+    }
+
+    # N.B. Placeholder. We will get this from dir name for Raw, and dir name prefix for Zarr,
+    #      when directory-parsing is implemented
+    timestamp = "220228-103800"
+    created_time = parse_timestamp(timestamp)
+
+    return RawDataset(
+        description=json_data["Description"],
+        data_classification=None,
+        directory=None,
+        users=None,
+        groups=None,
+        immutable=False,
+        identifiers=[
+            json_data["Basename"]["Sequence"],
+            str(json_data["SequenceID"]),
+        ],
+        experiments=[
+            json_data["Basename"]["Sample"],
+        ],
+        instrument=abi_music_microscope,
+        metadata=metadata,
+        schema=dataset_schema_raw,
+        created_time=created_time,
+        modified_time=None,
+    )
+
+
+def main() -> None:
+    """
+    main function - this is just for testing - a proper ingestion runner is yet to be written.
+    """
+
+    data_root = Path("/home/andrew/dev/ro-crate-abi-music/JSON templates/Test/project")
+
+    project_json_path = data_root / "project.json"
+    with project_json_path.open(encoding="utf-8") as f:
+        project_data = json.load(f)
+
+    raw_project = parse_project_info(json_data=project_data)
+    print("Raw Project:\n", raw_project.model_dump_json(indent=4))
+
+    experiment_json_path = data_root / "sample" / "experiment.json"
+    with experiment_json_path.open(encoding="utf-8") as f:
+        experiment_data = json.load(f)
+
+    raw_experiment = parse_experiment_info(experiment_data)
+    print("Raw Experiment:\n", raw_experiment.model_dump_json(indent=4))
+
+    dataset_json_path = data_root / "sample" / "Ganglia561" / "Ganglia561.json"
+    with dataset_json_path.open(encoding="utf-8") as f:
+        dataset_data = json.load(f)
+
+    raw_dataset = parse_dataset_info(dataset_data)
+    print("Raw Dataset:\n", raw_dataset.model_dump_json(indent=4))
+
+    print("Done")
+
+
+if __name__ == "__main__":
+    main()
