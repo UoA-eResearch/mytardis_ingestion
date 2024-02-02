06bce45412c4eacdb49aefdd4cf3bb5953752030
JLoveUOA <152220301+JLoveUOA@users.noreply.github.com>
2024-02-02 13:57:07 +1300

    Ids 522 redefine ro crate as metadata extractor (#385)
    * added extractor and changed ingest script
    
    * bamboozle pylint from calling my duplicated imports duplicate

--- samples/ro_crate_ingestion_example.py
+++ samples/ro_crate_ingestion_example.py
@@ -8,12 +8,11 @@ from pathlib import Path
 from src.config.config import ConfigFromEnv
 from src.conveyor.conveyor import Conveyor
 from src.conveyor.transports.rsync import RsyncTransport
-from src.extraction.ingestibles import IngestibleDataclasses
 from src.ingestion_factory.factory import IngestionFactory
 from src.mytardis_client.mt_rest import MyTardisRESTFactory
 from src.overseers.overseer import Overseer
 from src.profiles.profile_loader import ProfileLoader
-from src.profiles.ro_crate.ro_crate_parser import ROCrateParser
+from src.profiles.ro_crate.ro_crate_parser import ROCrateExtractor
 from src.smelters.smelter import Smelter
 
 root = logging.getLogger()
@@ -39,18 +38,13 @@ PROFILE_NAME = "ro_crate"
 profile_loader = ProfileLoader(PROFILE_NAME)
 
 
-def ingest_data(
-    ro_crate_path: Path, rsync_pth: Path, crate_to_tardis_schema: Path
-) -> None:
+def ingest_data(ro_crate_path: Path, rsync_pth: Path) -> None:
     """
     Runs the ingestion pipeline.
     """
-    crate_parser = ROCrateParser(
-        ro_crate_path, crate_to_tardis_schema, schema_config=config.default_schema
-    )
+    crate_extractor = ROCrateExtractor()
+    metadata = crate_extractor.extract(ro_crate_path)
     # Extraction Plant
-    ingestible_dataclasses = IngestibleDataclasses()
-    ingestible_dataclasses = crate_parser.run_extraction(ingestible_dataclasses)
 
     # Ingestion Factory
     mt_rest = MyTardisRESTFactory(config.auth, config.connection)
@@ -70,12 +64,12 @@ def ingest_data(
         smelter=smelter,
     )
 
-    factory.ingest_projects(ingestible_dataclasses.get_projects())
-    factory.ingest_experiments(ingestible_dataclasses.get_experiments())
-    factory.ingest_datasets(ingestible_dataclasses.get_datasets())
-    factory.ingest_datafiles(ingestible_dataclasses.get_datafiles())
+    factory.ingest_projects(metadata.get_projects())
+    factory.ingest_experiments(metadata.get_experiments())
+    factory.ingest_datasets(metadata.get_datasets())
+    factory.ingest_datafiles(metadata.get_datafiles())
 
-    datafiles = ingestible_dataclasses.get_datafiles()
+    datafiles = metadata.get_datafiles()
     rsync_transport = RsyncTransport(Path(rsync_pth))
     conveyor = Conveyor(rsync_transport)
     conveyor_process = conveyor.initiate_transfer(Path(ro_crate_path).parent, datafiles)
@@ -93,17 +87,13 @@ if __name__ == "__main__":
             "python ingestion_ro_crate_example.py "
             '"/path/to/ro_crate/data" '
             '"/path/to/the/rsync/destination"'
-            '"/path/to/the/schema/mapping"'
         ),
     )
     parser.add_argument("ro_crate_path", type=str, help="Path to the ro_crate file")
     parser.add_argument("rsync_pth", type=str, help="Path to the rsync destination")
-    parser.add_argument(
-        "schema_path", type=str, help="Path to the ro_crate to my tardis mapping schema"
-    )
 
     if len(sys.argv) == 1:
         parser.print_help()
         sys.exit(1)
     args = parser.parse_args()
-    ingest_data(args.ro_crate_path, args.rsync_pth, args.schema_path)
+    ingest_data(args.ro_crate_path, args.rsync_pth)
