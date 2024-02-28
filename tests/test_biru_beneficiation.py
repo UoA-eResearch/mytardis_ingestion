aa7a56fa245e3d6b345dd43f4369aa629785e713
Noel <noel.zeng@auckland.ac.nz>
2024-02-28 16:24:58 +1300

    Fix BIRU test

--- tests/test_biru_beneficiation.py
+++ tests/test_biru_beneficiation.py
@@ -1,8 +1,5 @@
 import logging
-import tempfile
-
-import yaml
-from pytest import fixture
+from pathlib import Path
 
 from src.extraction.manifest import IngestionManifest
 from src.profiles.idw.custom_beneficiation import CustomBeneficiation
@@ -12,25 +9,14 @@ logger.propagate = True
 
 
 def test_beneficiate():
-    with tempfile.NamedTemporaryFile(
-        mode="w", delete=False, suffix=".yaml"
-    ) as tmp_yaml_file:
-        yaml_data = {"key1": "value1", "key2": "value2"}
-        yaml.dump(yaml_data, tmp_yaml_file, default_flow_style=False)
+    """Tests that beneficiation for BIRU is successful and extracts metadata.
+    """
     a = CustomBeneficiation()
-    fpath = "tests/testdata/test_ingestion.yaml"
-    ingestible_dataclasses = a.beneficiate(fpath, IngestionManifest)
+    fpath = "tests/testdata/"
+    ingestible_dataclasses = a.beneficiate(Path(fpath), IngestionManifest())
     df = ingestible_dataclasses.get_datafiles()[0]
     assert df.filename == "20221113_slide3-2_humanRWM_cd34_x20_0.12umpix_3.czi"
-    assert df.metadata["Experimenter|UserName"] == "hsuz002"
-
-
-def test_beneficiate_replace_micrometer():
-    a = CustomBeneficiation()
-    fpath = "tests/testdata/test_ingestion.yaml"
-    ingestible_dataclasses = a.beneficiate(fpath, IngestionManifest)
-    df = ingestible_dataclasses.get_datafiles()[0]
-    assert df.filename == "20221113_slide3-2_humanRWM_cd34_x20_0.12umpix_3.czi"
-    assert df.metadata["Image|Pixels|Channel|Channel:0:0|PinholeSizeUnit"] == "um"
-    assert df.metadata["Image|Pixels|Channel|Channel:0:1|PinholeSizeUnit"] == "um"
+    assert df.metadata and df.metadata["Experimenter|UserName"] == "hsuz002"
+    assert df.metadata and df.metadata["Image|Pixels|Channel|Channel:0:0|PinholeSizeUnit"] == "um"
+    assert df.metadata and df.metadata["Image|Pixels|Channel|Channel:0:1|PinholeSizeUnit"] == "um"
     assert df.md5sum == "66dae4208956c7b0081bd31acae35506"
