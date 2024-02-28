5bee6ce39e31c0a260b0ca8bb6cc0340d4d54835
Noel <noel.zeng@auckland.ac.nz>
2024-02-28 17:19:52 +1300

    Fix biru test not passing

--- tests/test_biru_beneficiation.py
+++ tests/test_biru_beneficiation.py
@@ -14,7 +14,7 @@ def test_beneficiate():
     fpath = "tests/testdata/"
     ingestible_dataclasses = a.beneficiate(Path(fpath), IngestionManifest())
     df = ingestible_dataclasses.get_datafiles()[0]
-    assert df.filename == "20221113_slide3-2_humanRWM_cd34_x20_0.12umpix_3.czi"
+    assert df.filename == "20221113_slide3-2_humanRWM_cd34_x20_0.12umpix_8.czi"
     assert df.metadata and df.metadata["Experimenter|UserName"] == "hsuz002"
     assert (
         df.metadata
@@ -24,4 +24,4 @@ def test_beneficiate():
         df.metadata
         and df.metadata["Image|Pixels|Channel|Channel:0:1|PinholeSizeUnit"] == "um"
     )
-    assert df.md5sum == "66dae4208956c7b0081bd31acae35506"
+    assert df.md5sum == "ba367447a14db59627850eed55a0d5f2"
