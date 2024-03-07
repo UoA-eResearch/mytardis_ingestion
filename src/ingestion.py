10c928a2d6091975796f21686f9592a2967351c0
Noel <noel.zeng@auckland.ac.nz>
2024-03-07 16:29:15 +1300

    Fix pylint errors

--- src/ingestion.py
+++ src/ingestion.py
@@ -117,7 +117,7 @@ def upload(
     try:
         config = ConfigFromEnv(
             # Create storagebox config based on passed in argument.
-            source_directory=data_root,
+            source_directory=data_dir,
             storage=FilesystemStorageBoxConfig(
                 storage_name=storage_name, target_root_dir=storage_dir
             ),
