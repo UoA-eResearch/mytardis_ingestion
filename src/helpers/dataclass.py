2d12da12a4cb73096e501ec44185f6e507adea0b
Andrew Wilson <andrew.wilson@auckland.ac.nz>
2024-03-19 16:05:35 +1300

    Delete two unused functions

--- src/helpers/dataclass.py
+++ src/helpers/dataclass.py
@@ -1,72 +1,11 @@
-# pylint: disable=R0801
 """Helper functions related to multiple dataclasses."""
 
-from typing import List
 
-from src.blueprints.datafile import BaseDatafile, RawDatafile, RefinedDatafile
-from src.blueprints.dataset import (
-    BaseDataset,
-    DatasetParameterSet,
-    RawDataset,
-    RefinedDataset,
-)
-from src.blueprints.experiment import (
-    BaseExperiment,
-    ExperimentParameterSet,
-    RawExperiment,
-    RefinedExperiment,
-)
-from src.blueprints.project import (
-    BaseProject,
-    ProjectParameterSet,
-    RawProject,
-    RefinedProject,
-)
-from src.mytardis_client.enumerators import (
-    ObjectDict,
-    ObjectEnum,
-    ObjectPostDict,
-    ObjectPostEnum,
-)
-
-
-def get_object_type(
-    object_class: BaseDatafile | BaseDataset | BaseExperiment | BaseProject,
-) -> ObjectDict | None:
-    """Generic helper function to get the name from a MyTardis  dataclass object"""
-    if isinstance(object_class, BaseDatafile):
-        return ObjectEnum.DATAFILE.value
-    if isinstance(object_class, BaseDataset):
-        return ObjectEnum.DATASET.value
-    if isinstance(object_class, BaseExperiment):
-        return ObjectEnum.EXPERIMENT.value
-    if isinstance(object_class, BaseProject):
-        return ObjectEnum.PROJECT.value
-    return None
-
-
-def get_object_parents(
-    object_class: (
-        RawDatafile
-        | RefinedDatafile
-        | RawDataset
-        | RefinedDataset
-        | RawExperiment
-        | RefinedExperiment
-        | RawProject
-        | RefinedProject
-    ),
-) -> List[str] | None:  # sourcery skip: assign-if-exp, reintroduce-else
-    """Function to get the parent objects from teh RawObject classes."""
-    if isinstance(object_class, BaseDatafile):
-        return [object_class.dataset]
-    if isinstance(object_class, BaseDataset):
-        return object_class.experiments
-    if isinstance(object_class, BaseExperiment):
-        return object_class.projects
-    if isinstance(object_class, BaseProject):
-        return None
-    return []
+from src.blueprints.datafile import BaseDatafile
+from src.blueprints.dataset import BaseDataset, DatasetParameterSet
+from src.blueprints.experiment import BaseExperiment, ExperimentParameterSet
+from src.blueprints.project import BaseProject, ProjectParameterSet
+from src.mytardis_client.enumerators import ObjectPostDict, ObjectPostEnum
 
 
 def get_object_post_type(  # pylint: disable=too-many-return-statements
