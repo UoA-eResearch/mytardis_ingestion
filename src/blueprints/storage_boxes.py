62061e29c92fe637855c63ad52f71c17367da8e8
andrew-uoa <142769327+andrew-uoa@users.noreply.github.com>
2024-05-22 16:43:38 +1200

    Delete unused code (#424)
    * Delete unused code
    
    * Delete unused code
    
    * Delete unused code

--- src/blueprints/storage_boxes.py
+++ src/blueprints/storage_boxes.py
@@ -3,9 +3,8 @@
 
 from enum import Enum
 from pathlib import Path
-from typing import Dict, Optional
 
-from pydantic import BaseModel, ValidationInfo, field_validator
+from pydantic import BaseModel
 
 from src.blueprints.custom_data_types import URI
 
@@ -16,29 +15,6 @@ class StorageTypesEnum(Enum):
 
     FILE_SYSTEM = "file_system"
     S3 = "s3"
-
-
-class RawStorageBox(BaseModel):
-    """A storage box class to hold the name, location, uri and description
-    of a MyTardis storage box object"""
-
-    name: str
-    storage_class: str = "django.core.files.storage.FileSystemStorage"
-    description: str = ""
-    options: Dict[str, str]
-    attributes: Optional[Dict[str, str]] = None
-
-    @field_validator("description")
-    @classmethod
-    def get_description(  # pylint: disable=E0213
-        cls,
-        description: str,
-        info: ValidationInfo,
-    ) -> str:
-        """Generate a default description if one is not provided."""
-        if not description and "name" in info.data:
-            return f'Storage box for {info.data["name"]}'
-        return description
 
 
 class StorageBox(BaseModel):
