ec5b1008ec418ac8dec6f6279a2fdeb45a4f6254
Andrew Wilson <andrew.wilson@auckland.ac.nz>
2024-07-31 11:31:43 +1200

    Introduce a base dataclass to be used for all the validation dataclasses representing MyTardis resources

--- src/mytardis_client/response_data.py
+++ src/mytardis_client/response_data.py
@@ -1,18 +1,27 @@
 """Dataclasses for validating/storing MyTardis API response data."""
 
-from typing import Optional
+from typing import Any, Optional
 
 from pydantic import BaseModel, ConfigDict, Field, model_validator
 from typing_extensions import Self
 
+from src.mytardis_client.endpoints import URI
 from src.mytardis_client.objects import MyTardisObject
 
 
-class MyTardisIntrospection(BaseModel):
-    """MyTardis introspection data.
+class MyTardisResourceBase(BaseModel):
+    """Base class for data retrieved from MyTardis, associated with an ingested object."""
 
-    NOTE: this class relies on data from the MyTardis introspection API and therefore
-    can't be instantiated without a request to the specific MyTardis instance.
+    id: int
+    resource_uri: URI
+
+
+class MyTardisIntrospection(MyTardisResourceBase):
+    """MyTardis introspection data (the configuration of the MyTardis instance).
+
+    NOTE: this class relies on data from the MyTardis introspection API and
+    therefore can't be instantiated without a request to the specific MyTardis
+    instance.
     """
 
     model_config = ConfigDict(use_enum_values=False)
@@ -28,6 +37,23 @@ class MyTardisIntrospection(BaseModel):
     old_acls: bool = Field(validation_alias="experiment_only_acls")
     projects_enabled: bool
     profiles_enabled: bool
+
+    @model_validator(mode="before")
+    @classmethod
+    def validate_model(cls, data: Any) -> Any:
+        """Adapter for the raw data, as the ID from the introspection endpoint
+        comes back as None"""
+
+        if isinstance(data, dict):
+            dummy_id = 0
+            resource_uri = data.get("resource_uri")
+            if isinstance(resource_uri, str):
+                data["resource_uri"] = resource_uri.replace("None", str(dummy_id))
+
+            if "id" not in data:
+                data["id"] = dummy_id
+
+        return data
 
     @model_validator(mode="after")
     def validate_consistency(self) -> Self:
