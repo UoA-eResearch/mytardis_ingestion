c8fdeddc48e7b267c59f457a3149d1a595bc75ae
Andrew Wilson <andrew.wilson@auckland.ac.nz>
2024-07-31 16:35:30 +1200

    Add dataclasses for validating/storing response data from the MyTardis API

--- src/mytardis_client/response_data.py
+++ src/mytardis_client/response_data.py
@@ -1,10 +1,17 @@
 """Dataclasses for validating/storing MyTardis API response data."""
 
+from pathlib import Path
 from typing import Any, Optional
 
 from pydantic import BaseModel, ConfigDict, Field, model_validator
 from typing_extensions import Self
 
+from src.mytardis_client.common_types import (
+    DataClassification,
+    ISODateTime,
+    MD5Sum,
+    MTUrl,
+)
 from src.mytardis_client.endpoints import URI
 from src.mytardis_client.objects import MyTardisObject
 
@@ -14,6 +21,38 @@ class MyTardisResourceBase(BaseModel):
 
     id: int
     resource_uri: URI
+
+
+class Group(MyTardisResourceBase):
+    """Metadata associated with a group in MyTardis."""
+
+    name: str
+
+
+class Facility(MyTardisResourceBase):
+    """Metadata associated with a facility in MyTardis."""
+
+    created_time: ISODateTime
+    manager_group: Group
+    modified_time: ISODateTime
+    name: str
+
+
+class Institution(MyTardisResourceBase):
+    """Metadata associated with an institution in MyTardis."""
+
+    aliases: list[str]
+    identifiers: list[str]
+    name: str
+
+
+class Instrument(MyTardisResourceBase):
+    """Metadata associated with an instrument in MyTardis."""
+
+    created_time: ISODateTime
+    facility: Facility
+    modified_time: ISODateTime
+    name: str
 
 
 class MyTardisIntrospection(MyTardisResourceBase):
@@ -70,3 +109,142 @@ class MyTardisIntrospection(MyTardisResourceBase):
             )
 
         return self
+
+
+class ParameterName(MyTardisResourceBase):
+    """Schema parameter information"""
+
+    full_name: str
+    immutable: bool
+    is_searchable: bool
+    name: str
+    parent_schema: URI = Field(serialization_alias="schema")
+    sensitive: bool
+    units: str
+
+
+class Replica(MyTardisResourceBase):
+    """Metadata associated with a Datafile replica in MyTardis."""
+
+    created_time: ISODateTime
+    datafile: URI
+    last_verified_time: ISODateTime
+    location: str
+    uri: str  # Note: not a MyTardis resource URI
+    verified: bool
+
+
+class Schema(MyTardisResourceBase):
+    """Metadata associated with a metadata schema in MyTardis."""
+
+    hidden: bool
+    immutable: bool
+    name: str
+    namespace: MTUrl  # e.g. "http://130.216.253.65/noel-test-exp-schema",
+    parameter_names: list[ParameterName]
+
+
+class StorageBoxOption(MyTardisResourceBase):
+    """Data associated with a storage box option in MyTardis."""
+
+    key: str
+    storage_box: URI
+    value: str
+    value_type: str
+
+
+class StorageBox(MyTardisResourceBase):
+    """Metadata associated with a storage box in MyTardis."""
+
+    attributes: list[str]
+    description: str
+    django_storage_class: str
+    max_size: Optional[int]
+    name: str
+    options: list[StorageBoxOption]
+    status: str
+
+
+class User(MyTardisResourceBase):
+    """Dataa associated with a user in MyTardis."""
+
+    email: Optional[str]
+    first_name: Optional[str]
+    groups: list[Group]
+    last_name: Optional[str]
+    username: str
+
+
+class ProjectParameterSet(MyTardisResourceBase):
+    """Metadata associated with a project parameter set in MyTardis."""
+
+
+class ExperimentParameterSet(MyTardisResourceBase):
+    """Metadata associated with an experiment parameter set in MyTardis."""
+
+
+class DatasetParameterSet(MyTardisResourceBase):
+    """Metadata associated with a dataset parameter set in MyTardis."""
+
+
+class DatafileParameterSet(MyTardisResourceBase):
+    """Metadata associated with a datafile parameter set in MyTardis."""
+
+
+class IngestedProject(MyTardisResourceBase):
+    """Metadata associated with a project that has been ingested into MyTardis."""
+
+    classification: DataClassification
+    description: str
+    identifiers: Optional[list[str]]
+    institution: list[URI]
+    locked: bool
+    name: str
+    principal_investigator: str
+
+
+class IngestedExperiment(MyTardisResourceBase):
+    """Metadata associated with an experiment that has been ingested into MyTardis."""
+
+    classification: int
+    description: str
+    identifiers: Optional[list[str]]
+    institution_name: str
+    projects: list[IngestedProject]
+    title: str
+
+
+class IngestedDataset(MyTardisResourceBase):
+    """Metadata associated with a dataset that has been ingested into MyTardis."""
+
+    classification: DataClassification
+    created_time: ISODateTime
+    description: str
+    directory: Path
+    experiments: list[URI]
+    identifiers: list[str]
+    immutable: bool
+    instrument: Instrument
+    modified_time: ISODateTime
+    parameter_sets: list[DatasetParameterSet]
+    public_access: bool
+
+
+class IngestedDatafile(MyTardisResourceBase):
+    """Metadata associated with a datafile that has been ingested into MyTardis."""
+
+    created_time: Optional[ISODateTime]
+    dataset: URI
+    deleted: bool
+    deleted_time: Optional[ISODateTime]
+    directory: Path
+    filename: str
+    identifiers: Optional[list[str]]
+    md5sum: MD5Sum
+    mimetype: str
+    modification_time: Optional[ISODateTime]
+    parameter_sets: list[DatafileParameterSet]
+    public_access: bool
+    replicas: list[Replica]
+    size: int
+    version: int
