7e31e31781964c6f6ac13e22ab7df83e4ec1df9f
Andrew Wilson <andrew.wilson@auckland.ac.nz>
2024-07-31 15:02:28 +1200

    Use the ISO datetime parser from dateutils, instead of a hand-rolled regex

--- src/utils/validate.py
+++ src/utils/validate.py
@@ -1,8 +1,8 @@
 """Utility functions for validating data."""
 
-import re
 from typing import Any
 
+from dateutil import parser
 from validators import url
 
 
@@ -16,23 +16,22 @@ def is_hex(value: str) -> bool:
     return True
 
 
-iso_time_regex = re.compile(
-    r"^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(\.[0-9]+)?(Z|[+-](?:2[0-3]|[01][0-9]):[0-5][0-9])?$"  # pylint: disable=line-too-long
-)
-
-
 def validate_isodatetime(value: Any) -> str:
     """Custom validator to ensure that the value is a string object and that it matches
     the regex defined for an ISO 8601 formatted datetime string"""
     if not isinstance(value, str):
         raise TypeError(f'Unexpected type for ISO date/time stamp: "{type(value)}"')
-    if match := iso_time_regex.fullmatch(value):
-        return f"{match.group(0)}"
-    raise ValueError(
-        'Passed string value "%s" is not an ISO 8601 formatted '
-        "date/time string. Format should follow "
-        "YYYY-MM-DDTHH:MM:SS.SSSSSS+HH:MM convention" % (value)
-    )
+
+    try:
+        _ = parser.isoparse(value)
+    except ValueError as e:
+        raise ValueError(
+            f'Passed string value "{value}" is not an ISO 8601 formatted '
+            "date/time string. Format should follow "
+            "YYYY-MM-DDTHH:MM:SS.SSSSSS+HH:MM convention"
+        ) from e
+
+    return value
 
 
 def validate_url(value: Any) -> str:
